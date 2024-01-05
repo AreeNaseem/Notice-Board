5972c8bee4e986f33d50d467393e0d5c10a6c8e4
Areenaseem <aryn.nasym@gmail.com>
2024-01-06 06:28:20 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout meta-title="Laravel Blog - About us">
+
+    <div class="container mx-auto flex flex-wrap py-6">
+
+        <!-- Post Section -->
+        <section class="w-full md:w-full flex flex-col items-center px-3">
+            <h2 class="font-bold text-lg">Contact Details</h2>
+            <!-- Add your contact details here -->
+            <!-- Example: -->
+            <p class="text-sm mt-2">Avas Ride Office: 3323030</p>
+            <p class="text-sm">Avas Ride Boss: 7773333</p>
+            <p class="text-sm">Avas Ride HR: 3337676</p>
+            <p class="text-sm">HR Mail Address: Hr@avasride.com</p>
+            <p class="text-sm">Avas Ride Mail: admin@avasride.com</p>
+
+            <p class="text-sm mt-2"> އައް މެސެޖް ކޮއްލަން ބޭނުންވާނަމަ 102050191 މި ނަންބަރައް ޓެލެގްރާމް އިން މެސެޖް ކޮށްދެއްވާ HR</p>
+
+        
+        </section>
+
+    </div>
+</x-app-layout>
